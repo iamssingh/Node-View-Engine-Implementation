6926659d4a89d678b5d7b6b8f6743c370c0a103d
SSingh883710 <ssingh883710@gmail.com>
2022-04-25 12:33:58 +0530

    ejs implemented.

--- src/app.js
+++ src/app.js
@@ -3,9 +3,14 @@ const path = require('path');
 
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
-    response.send('Hello World');
+    response.render('index', {
+        subject: 'EJS template engine',
+        name: 'our template',
+        link: 'https://google.com'
+    });
 });
 
 app.listen(5000, () => {
